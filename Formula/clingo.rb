6707a40986913658f895ef928297b716f3a866d4
Dominik Moritz <domoritz@gmail.com>
2021-01-02 21:38:03 +0100

    clingo 5.4.1

--- Formula/clingo.rb
+++ Formula/clingo.rb
@@ -1,8 +1,8 @@
 class Clingo < Formula
   desc "ASP system to ground and solve logic programs"
   homepage "https://potassco.org/"
-  url "https://github.com/potassco/clingo/archive/v5.4.0.tar.gz"
-  sha256 "e2de331ee0a6d254193aab5995338a621372517adcf91568092be8ac511c18f3"
+  url "https://github.com/potassco/clingo/archive/v5.4.1.tar.gz"
+  sha256 "ac6606388abfe2482167ce8fd4eb0737ef6abeeb35a9d3ac3016c6f715bfee02"
   license "MIT"
   revision 3
 
@@ -30,12 +30,6 @@ class Clingo < Formula
   link_overwrite "bin/gringo"
   link_overwrite "bin/lpconvert"
   link_overwrite "bin/reify"
-
-  # Patch adds Python 3.8 compatibility
-  patch do
-    url "https://github.com/potassco/clingo/commit/13a896b6a762e48c2396e3dc9f2e794020f4e6e8.patch?full_index=1"
-    sha256 "f2840bc4ab5159c253c5390c845f0d003f5fef813f49ddca27dbd5245f535e79"
-  end
 
   def install
     system "cmake", ".", "-DCLINGO_BUILD_WITH_PYTHON=ON",
